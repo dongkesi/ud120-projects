cddc91d29e610f2f6e09030f270015889dda7179
Dongkesi <skd2278@gmail.com>
2017-10-03 16:34:59 +0800

    SVM trial

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,10 +20,33 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+# 0.616040955631
+# 0.616040955631
+# 0.821387940842
+# 0.892491467577
+clf = SVC(C=10000.0, kernel='rbf')
+t0 = time()
+clf.fit(features_train, labels_train)
+print 'train time:', round(time()-t0, 3), 's'
+t0 = time()
+acc = clf.score(features_test, labels_test)
+print 'test time:', round(time()-t0, 3), 's'
+print 'acc:', acc
+# pred = clf.predict([features_test[10], features_test[26], features_test[50]])
+pred = clf.predict(features_test)
+print(pred)
+cnt = 0
+for i in range(len(pred)):
+    if pred[i] == 1:
+       cnt += 1
+print cnt
 
 #########################################################
 
